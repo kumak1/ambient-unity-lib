5edffdfba1040557e6cf60a348e2ddaff03c3be7
kumak1 <makino@pepabo.com>
2022-02-28 02:39:17 +0900

    update package

---
+++ AmbientReadParameter.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Web;
+
+namespace AmbientUnityLib
+{
+    public class AmbientReadParameter
+    {
+        public DateTime? Date = null;
+        public DateTime? Start = null;
+        public DateTime? End = null;
+
+        public AmbientReadParameter()
+        {
+        }
+
+        public NameValueCollection ToCollection()
+        {
+            var query = HttpUtility.ParseQueryString(string.Empty);
+
+            if (Date != null)
+            {
+                query.Add("date", Date.ToString());
+                return query;
+            }
+
+            if (Start != null && End != null)
+            {
+                query.Add("start", Start.ToString());
+                query.Add("end", End.ToString());
+                return query;
+            }
+
+            return query;
+        }
+
+        public NameValueCollection ToCollectionWithKey(string readKey)
+        {
+            var param = ToCollection();
+            param.Add("readKey", readKey);
+            return param;
+        }
+    }
+}
